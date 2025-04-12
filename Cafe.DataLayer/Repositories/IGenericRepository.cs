f83d14242eb8abc2669c204378fa9f5fc7fe3d22
Kooroshh <kooroshsoleymani98@gmail.com>
2025-04-12 16:43:28 +0330

    Add UOF To Project

---
+++ Cafe.DataLayer/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.DataLayer.Repositories
+{
+    internal interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> where = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderby = null);
+        TEntity GetById(object key);
+        void Delete(TEntity entity);
+        void Delete(object key);
+        void Update(TEntity entity);
+        void Create(TEntity entity);
+    }
+}
--- Cafe/Products/Products.Designer.cs
+++ Cafe/Products/Products.Designer.cs
@@ -28,22 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAddProduct = new System.Windows.Forms.ToolStripButton();
+            this.btnEditProduct = new System.Windows.Forms.ToolStripButton();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
             this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAddProduct,
+            this.btnEditProduct});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(582, 67);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAddProduct
+            // 
+            this.btnAddProduct.Image = global::Cafe.Properties.Resources._1371475930_filenew;
+            this.btnAddProduct.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnAddProduct.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAddProduct.Name = "btnAddProduct";
+            this.btnAddProduct.Size = new System.Drawing.Size(111, 64);
+            this.btnAddProduct.Text = "افزودن محصول";
+            this.btnAddProduct.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // btnEditProduct
+            // 
+            this.btnEditProduct.Image = global::Cafe.Properties.Resources._1371475973_document_edit;
+            this.btnEditProduct.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnEditProduct.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnEditProduct.Name = "btnEditProduct";
+            this.btnEditProduct.Size = new System.Drawing.Size(114, 64);
+            this.btnEditProduct.Text = "ویرایش محصول";
+            this.btnEditProduct.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.AllowUserToAddRows = false;
+            this.dgvProducts.AllowUserToDeleteRows = false;
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvProducts.Location = new System.Drawing.Point(0, 67);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.ReadOnly = true;
+            this.dgvProducts.RowHeadersWidth = 51;
+            this.dgvProducts.RowTemplate.Height = 24;
+            this.dgvProducts.Size = new System.Drawing.Size(582, 336);
+            this.dgvProducts.TabIndex = 1;
             // 
             // Products
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 403);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.toolStrip1);
             this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Products";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "محصولات";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnAddProduct;
+        private System.Windows.Forms.ToolStripButton btnEditProduct;
+        private System.Windows.Forms.DataGridView dgvProducts;
     }
 }
